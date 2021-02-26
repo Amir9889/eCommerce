592755b87996711a255812268b1309e714afd9ac
Ameer <developer.app9889@gmail.com>
2021-02-26 03:51:07 +0330

    Episode 3 Commit

--- app/src/main/java/com/example/ecommerce/MainActivity.java
+++ app/src/main/java/com/example/ecommerce/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.ecommerce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button joinNowBtn, loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeUIElements();
+    }
+
+    private void initializeUIElements() {
+        joinNowBtn = findViewById(R.id.main_join_now_btn);
+        loginBtn = findViewById(R.id.main_login_btn);
     }
 }
